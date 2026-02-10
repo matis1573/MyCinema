df04768ee73360a61c6e4abf717e28ff2ac45253
test <mehwa.mnz@icloud.com>
2026-02-10 19:45:20 +0100

    push all

--- backend/index.php
+++ backend/index.php
@@ -1,37 +1,60 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=mycinema;charset=utf8', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
+header('Content-Type: application/json; charset=utf-8');
+require_once 'database.php';
 require_once 'Movie.php';
 require_once 'MovieRepository.php';
 require_once 'MovieController.php';
-
 require_once 'Room.php';
 require_once 'RoomRepository.php';
 require_once 'RoomController.php';
-
 require_once 'Screening.php';
 require_once 'ScreeningRepository.php';
 require_once 'ScreeningController.php';
 
-
 $request = $_GET['action'] ?? '';
+$method = $_SERVER['REQUEST_METHOD'];
 
 switch ($request) {
+    // Liste
     case 'list_movies':
-        $controller = new MovieController();
-        $controller->list();
+        (new MovieController())->list();
         break;
-
     case 'list_rooms':
-        $controller = new RoomController();
-        $controller->list();
+        (new RoomController())->list();
+        break;
+    case 'list_screenings':
+        (new ScreeningController())->list();
         break;
 
-    case 'list_screenings':
-        $controller = new ScreeningController();
-        $controller->list();
+    // Ajout
+    case 'add_movie':
+        if ($method === 'POST') {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $movie = new Movie($data['title'], $data['duration'], $data['description'], $data['release_year'], $data['genre'], $data['director']);
+            $repo = new MovieRepository();
+            $repo->add($movie);
+            echo json_encode(["success" => true]);
+        }
+        break;
+
+    case 'add_room':
+        if ($method === 'POST') {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $room = new Room($data['name'], $data['capacity']);
+            $repo = new RoomRepository();
+            $repo->add($room);
+            echo json_encode(["success" => true]);
+        }
+        break;
+
+    case 'add_screening':
+        if ($method === 'POST') {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $screening = new Screening($data['movie_id'], $data['room_id'], $data['start_time']);
+            $repo = new ScreeningRepository();
+            $repo->add($screening);
+            echo json_encode(["success" => true]);
+        }
         break;
 
     default:
